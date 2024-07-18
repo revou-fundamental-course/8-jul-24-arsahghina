62f6be4334253acf527082635cc23679acc9c65a
Ghina Salwa Arsah <ghinasalwaarsah2004@gmail.com>
2024-07-18 22:01:04 +0700

    update js css & html

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function updateResult() {
+    let weightValue = document.querySelector('#input-berat-badan').value;
+    console.log(weightValue);
+    // jika weightValue tidak sama dengan null dan wajib weightValue tidak sama dengan ''
+    weightValue != null && weightValue != '' 
+    ? document.querySelector('#bmi-result').innerHTML = weightValue 
+    : alert('inputan anda kosong!');
+}
